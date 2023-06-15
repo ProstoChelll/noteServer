dddb25f71c500de2b5173277696ba194fce8c5e5
Tim-KILLER <abdulmanovtumur@mail.ru>
2023-06-15 15:38:53 +0300

    bag

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const app = express();
 const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
 MongoClient.connect(db.url, (err, database) => {
   if (err) return console.log(err);
   require("./app/routes")(app, database);
